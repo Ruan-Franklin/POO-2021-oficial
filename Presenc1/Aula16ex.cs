a851c194d73b4fee2396680814e8d137826a3b25
RUAN FRANKLIN <94229859+Ruan-Franklin@users.noreply.github.com>
2022-03-07 18:14:16 +0000

    Alterações feitas durante a aula presencial.

---
+++ Presenc1/Aula16ex.cs
@@ -0,0 +1,13 @@
+using System;
+class Program{
+  public static void Main(string [] args){
+    Figura a= new Figura();
+    
+  }
+}
+
+abstract class Figura3D{
+  public abstract double GetVolume();
+}
+
+class Esfera
